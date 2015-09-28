6de1de3255101df5e9c52c7a8d1ade555368786b
ashwin47 <thatsashwin@gmail.com>
2015-09-28 15:13:46 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<script>
+  window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '1058706867472639',
+      xfbml      : true,
+      version    : 'v2.4'
+    });
+  };
+
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+</script>
